87b9b65919fe9a88c2efd05e44465e444e738728
Vishwanath-Ramesh <vishwanathr.dev@outlook.com>
2022-03-30 20:22:05 +0530

    fix: Removed the mongodb support to seperate branch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const mongoClient = require('./service/database')
 
 const app = express()
 const port = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(cors({ origin: true }))
-
-mongoClient.connect()
 
 app.get('/api', (req, res) => {
   res.status(200).send({ result: 'SUCCESS' })
